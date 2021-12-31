4843c2ae1d9b1a3047fb9bea52fa83caf695504d
jsconan <jsconan@gmail.com>
2021-12-31 22:17:38 +0100

    feat: add slicer script

---
+++ rcmodels/tracks/slice.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# GPLv3 License
+#
+# Copyright (c) 2020 Jean-Sebastien CONAN
+#
+# This file is part of jsconan/things.
+#
+# jsconan/things is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# jsconan/things is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with jsconan/things. If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# A race track system for 1/24 to 1/32 scale RC cars.
+#
+# Slices the STL files for the project.
+#
+# @author jsconan
+#
+
+# script config
+configpath="config/config.ini"
+configdefault="config/config-dist.ini"
+
+# defines the config path
+if [ "${configpath}" != "" ] && [ ! -f "${configpath}" ]; then
+    printmessage "${C_ERR}Warning! The config for Slic3r does not exist.\nWill use the default one"
+    cp "${configdefault}" "${configpath}"
+fi
+
+# redirect to the lib utils
+"$(dirname $0)/../../lib/camelSCAD/scripts/slice.sh" \
+    --input "output" \
+    --output "dist" \
+    --config "${configpath}" \
+    --prusaslicer \
+    --recurse \
+    "$@"
