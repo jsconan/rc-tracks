31958150e2cfe6eb9a3f26d7d17d3762972e3200
jsconan <jsconan@gmail.com>
2022-01-07 00:03:35 +0100

    chore: complete the import by updating the file headers

--- scale-24/slice.sh
+++ scale-24/slice.sh
@@ -2,22 +2,22 @@
 #
 # GPLv3 License
 #
-# Copyright (c) 2020 Jean-Sebastien CONAN
+# Copyright (c) 2020-2022 Jean-Sebastien CONAN
 #
-# This file is part of jsconan/things.
+# This file is part of jsconan/rc-tracks.
 #
-# jsconan/things is free software: you can redistribute it and/or modify
+# jsconan/rc-tracks is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# jsconan/things is distributed in the hope that it will be useful,
+# jsconan/rc-tracks is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with jsconan/things. If not, see <http://www.gnu.org/licenses/>.
+# along with jsconan/rc-tracks. If not, see <http://www.gnu.org/licenses/>.
 #
 
 #
